34796310e974ff5229a628411d7c430fc7f0fbff
ViniWixak <viniciuswixak@gmail.com>
2022-05-17 09:36:57 -0300

    fixing bugs

--- scripts.js
+++ scripts.js
@@ -3,20 +3,22 @@ const animacaoClasse = 'animacao';
 const btn = document.querySelector('button');
 const inputs = document.querySelector('form');
 
-animaScroll();
-
 function animaScroll(){
-    const topoPaginaNaJanela = window.pageYOffset + ((window.innerHeight*2/4));
+    const topoPaginaNaJanela = window.pageYOffset + ((window.innerHeight*3/4));
     elementos.forEach(function(elemento){
         if(topoPaginaNaJanela > elemento.offsetTop){
             elemento.classList.add(animacaoClasse);
         }else{
             elemento.classList.remove(animacaoClasse);
         }
+        
     });
 }
 
-if(elementos.length){
+if(elementos.length ){
+    window.onload = (event) => {
+        animaScroll();
+    };
     window.addEventListener('scroll', function(){
         animaScroll();
     })
